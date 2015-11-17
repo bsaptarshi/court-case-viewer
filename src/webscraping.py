ea74bd048e63c8cba4ba7ea8f32a1ea019818611
Saptarshi Bhattacharjee <saptarsh@buffalo.edu>
2015-11-17 15:31:16 -0500

    header parsing

--- src/webscraping.py
+++ src/webscraping.py
@@ -1,7 +1,9 @@
 from lxml import html
 import requests
 from bs4 import BeautifulSoup
+import re
 
+headerData = {}
 def scrapehelper():
 	r0 = requests.get ('http://clists.nic.in/viewlist/index.php?court=VTNWd2NtVnRaU0JEYjNWeWRDQnZaaUJKYm1ScFlRPT0=&q=TkRZeU5UQXpaV1kwWldNeVpHWmlOVGxoWXpFNFlqRXdOVE5pWmpNd00yVT0=')
 	sessionID = r0.headers['Set-Cookie'].split('=', 1)[1].split(';')[0]
@@ -16,14 +18,22 @@ def scrapehelper():
 def parseHTMLtoJSON(htmlText):
 	# print htmlText
 	soup = BeautifulSoup(htmlText, 'html.parser')
-	# print soup.prettify()
 
 	tables = soup.findChildren('table')
-	print len(tables)
-	# print tables[0]
-	# print tables[1]
-	# print tables[2]
-	# print tables[3]
-	print tables[4].prettify()
+	storeHeader(tables[0])
+
+	
+
+def storeHeader(headerText):
+	children = headerText.find('td')
+	contentText = re.sub('<[^<]+?>', '&&&', children.renderContents().strip()).split('&&&')
+	courtNo = contentText[0].split(' ')[2].strip()
+	justice1 = contentText[1].strip()
+	justice2 = contentText[2].strip()
+
+	headerData["CourtNo"] = courtNo
+	headerData["Justice1"] = justice1
+	headerData["Justice2"] = justice2
+
 if __name__ == '__main__':
 	scrapehelper()
