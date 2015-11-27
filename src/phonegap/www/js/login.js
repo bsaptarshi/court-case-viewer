d03c59d45cc950fe722348496d31725872cd85ba
Srinivasan Rajappa <srajappa@zappos.com>
2015-11-26 20:22:11 -0500

    added akash's modifications

---
+++ src/phonegap/www/js/login.js
@@ -0,0 +1,26 @@
+$(function(){
+	$("#loginForm").submit(function(event){
+		event.preventDefault();
+		var username = $("#username").val();
+		var password = $("#password").val();
+		var url = localStorage.getItem("baseUrl")+"/home/login/";		
+		var data  = {"username":username,"password":password};
+		$.ajax({
+		  type: "POST",
+		  url: url,
+		  data: data,
+		  success: success,
+		  error: error,
+		});
+		function success(data){
+			alert("Login successfull");
+			localStorage.setItem("username",username);
+			 window.location="home.html";
+		}
+		function error(){
+			alert("Login failed");			
+		}
+	});
+
+
+});
